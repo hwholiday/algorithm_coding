205e00148c80e9fa8e6f3f5e0c3c83a35b7da8de
hero <king>
2019-08-14 13:58:58 +0800

    添加sego中文分词工具

--- similarity_code/cosine_test.go
+++ similarity_code/cosine_test.go
@@ -1,10 +1,24 @@
 package similarity_code
 
 import (
-	"fmt"
+	"github.com/huichen/sego"
 	"testing"
 )
 
 func TestCosine(t *testing.T) {
-	fmt.Println(Cosine([]float64{1, 1, 1, 1, 1}, []float64{1, 0, 1, 1, 1, 1, 1, 1}))
+	t.Log(Cosine([]float64{1, 1, 1, 1, 1}, []float64{1, 0, 1, 1, 1, 1, 1, 1}))
+}
+
+func TestSego(t *testing.T) {
+	var segmenter sego.Segmenter
+	segmenter.LoadDictionary("./dictionary.txt")
+	segments := segmenter.Segment([]byte("梦是美好的吗?我不能确定，但也不是悲伤的。天使的彩色羽翼张开，羽毛飘落，每一片羽毛都有一个梦，你的梦是什么颜色的羽毛?"))
+	a := sego.SegmentsToSlice(segments, false)
+	b := sego.SegmentsToString(segments, true)
+	for _, v := range a {
+		t.Log("a >>>>>>", v)
+	}
+	for _, v := range b {
+		t.Log("b >>>>>>", v)
+	}
 }
