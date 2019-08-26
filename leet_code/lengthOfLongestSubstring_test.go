4ff94a7415631b47d653e1f47c431b12c149539f
Howie <hwholiday@163.com>
2019-08-26 23:01:50 +0800

    无重复字符的最长子串

--- leet_code/lengthOfLongestSubstring_test.go
+++ leet_code/lengthOfLongestSubstring_test.go
@@ -5,18 +5,23 @@ import (
 )
 
 func TestLengthOfLongestSubstring(t *testing.T) {
-	t.Log(lengthOfLongestSubstring("abcabcbb"))
-}
-
-type Data struct {
-	Index int
-	Val   int
-	have  bool
+	t.Log(lengthOfLongestSubstring("abcdefgafkzwb"))
 }
 
 func lengthOfLongestSubstring(s string) int {
-	if len(s) == 0 {
-		return 0
+	lastOccurred := make(map[byte]int)
+	start := 0
+	maxLength := 0
+	for i, ch := range []byte(s) {
+		//找到上次该字母出现的起点
+		//判断该位置是否大于起点
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastI + 1 //从该字母后一位作为起点,以前的存在相同的而且已经判断就不再判断
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
 	}
-	return 0
+	return maxLength
 }
