d34e0a1f1697db04d1fc74884f1f599185a9fb25
hero <king>
2019-09-03 16:45:01 +0800

    最长公共前缀

---
+++ leet_code/longestCommonPrefix_test.go
@@ -0,0 +1,33 @@
+package leet_code
+
+import (
+	"testing"
+)
+
+//最长公共前缀
+func TestLongestCommonPrefix(t *testing.T) {
+	t.Log(longestCommonPrefix([]string{"aa", "aa", "aa"}))
+}
+
+func longestCommonPrefix(strs []string) string {
+	/*if len(strs) <= 0 {
+		return ""
+	}
+	var (
+		result string
+		endFor bool
+	)
+	for _, v := range strs {
+
+	}
+	for index, v := range strs[0] {
+		result += string(v)
+			}
+		}
+		if endFor {
+			result = result[:len(result)-1]
+			break
+		}
+	}*/
+	return "result"
+}
