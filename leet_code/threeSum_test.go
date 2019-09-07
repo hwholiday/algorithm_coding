12a7c192952082bd3b75491ab7f492f252574cdf
hero <hw>
2019-09-08 00:42:06 +0800

    三数之和

--- leet_code/threeSum_test.go
+++ leet_code/threeSum_test.go
@@ -1,6 +1,7 @@
 package leet_code
 
 import (
+	"fmt"
 	"sort"
 	"testing"
 )
@@ -12,34 +13,39 @@ func TestThreeSum(t *testing.T) {
 
 func threeSum(nums []int) [][]int {
 	var (
-		result [][]int
+		result    [][]int
+		numsMap   = make(map[int]int, len(nums))
+		resultMap = make(map[string]string)
+		allIsZero = true
 	)
-	for i := 0; i < len(nums)-2; i++ {
-		for j := i + 1; j < len(nums)-1; j++ {
-			for k := j + 1; k < len(nums); k++ {
-				if nums[i]+nums[j]+nums[k] == 0 {
-					data := []int{nums[i], nums[j], nums[k]}
-					sort.Ints(data)
-					for _, v := range result {
-						if !equal(v, data) {
-							result = append(result, data)
-						}
+	if len(nums) < 3 {
+		return result
+	}
+	sort.Ints(nums)
+	for i, v := range nums {
+		numsMap[v] = i
+		if v != 0 {
+			allIsZero = false
+		}
+	}
+	if allIsZero {
+		result = append(result, []int{0, 0, 0})
+		return result
+	}
+
+	for i, v := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if index, ok := numsMap[0-(v+nums[j])]; ok {
+				data := []int{v, nums[j], 0 - (v + nums[j])}
+				sort.Ints(data)
+				if _, ok := resultMap[fmt.Sprintf("%d_%d_%d", data[0], data[1], data[2])]; !ok {
+					if index != i && index != j {
+						result = append(result, data)
 					}
+					resultMap[fmt.Sprintf("%d_%d_%d", data[0], data[1], data[2])] = ""
 				}
 			}
 		}
 	}
 	return result
-}
-
-func equal(x, y []int) bool {
-	if len(x) != len(y) {
-		return false
-	}
-	for i := range x {
-		if x[i] != y[i] {
-			return false
-		}
-	}
-	return true
 }
