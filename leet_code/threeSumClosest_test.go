8f3ff6552c0b2b98437f65499af52d1fd100111d
hero <hw>
2019-09-10 22:44:15 +0800

    最接近的三数之和

---
+++ leet_code/threeSumClosest_test.go
@@ -0,0 +1,19 @@
+package leet_code
+
+import (
+	"log"
+	"testing"
+)
+
+// 最接近的三数之和
+func TestThreeSumClosest(t *testing.T) {
+	log.Fatal(threeSumClosest([]int{-1, 2, 1, -4}, 1))
+}
+
+func threeSumClosest(nums []int, target int) int {
+	var (
+		result int
+	)
+
+	return result
+}
