59bb966e812d43a8c1241b2f3724603665d6598a
hero <king>
2019-09-12 17:16:24 +0800

    合并两个有序链表

--- leet_code/mergeTwoLists_test.go
+++ leet_code/mergeTwoLists_test.go
@@ -4,11 +4,22 @@ import (
 	"testing"
 )
 
-//TODO 合并两个有序链表
+//合并两个有序链表
 func TestMergeTwoLists(t *testing.T) {
 
 }
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-
-	return nil
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
 }
