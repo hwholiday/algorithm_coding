30b9527b4b15f3eed055ce596a89a226eb2d71fc
hero <hero@520.com>
2020-12-15 18:28:28 +0800

    最小覆盖子串

--- leet_code/minWindow_test.go
+++ leet_code/minWindow_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-//最小覆盖子串
+//最小覆盖子串 暴力破解
 func minWindow(s string, t string) string {
 	var (
 		tMap = make(map[rune]int, len(t))
@@ -49,7 +49,48 @@ func minWindow(s string, t string) string {
 	}
 	return end
 }
+func minWindowV2(s string, t string) string {
+	var (
+		tMap = make(map[string]int, len(t))
+		sMap = make(map[string]int)
+	)
+	for _, v := range t {
+		tMap[string(v)]++
+	}
+	check := func() bool {
+		for k, v := range tMap {
+			val, ok := sMap[k]
+			if !ok || val < v {
+				return false
+			}
+		}
+		return true
+	}
+	var (
+		end    = s
+		endNum = len(s)
+		isFind = false
+	)
+	for l, r := 0, 0; r < len(s); r++ {
+		sMap[string(s[r])]++
+		if check() {
+			isFind = true
+			for check() {
+				sMap[string(s[l])]--
+				l++
+			}
+			if endNum > (r+1)-(l-1) {
+				endNum = (r + 1) - (l - 1)
+				end = s[l-1 : r+1]
+			}
+		}
+	}
+	if !isFind {
+		return ""
+	}
+	return end
+}
 
 func Test_minWindow(t *testing.T) {
-	t.Log(minWindow("qdsvbyuipqxnhkbgqdgozelvapgcainsofnrfjzvawihgmpwpwnqcylcnufqcsiqzwhhhjchfmqmagkrexigytklnrdslmkniuurdwzikrwlxhcbgkjegwsvnvpzhamrwgjzekjobizbreexqqewmwubtjadlowhwhiarurvcsyvwcunsylgwhisrivezrmvzwwsqppuhnreqmtkkgrjozbhjwlkpzgqwejotylamcgeqzobihmwinduloecqmtoqcejcpmqusukulncsbabodxbtbeloxzgbesdveupyocuzryutyxjdulzvpklokspqkslqodqfhlgajatkxfntqorhzcxlwmdigoyxtrcccidnlyxidnevdveczbpwpugyjhveyxhcfkpqipboehjhcombrdzhyghjncnnzwpggezrvcfzjqjngvoyyqhwwohlsvarrpzavatrcasnqbazyrzxhivfydsqasjtjiteloxposdhtfgswhrfpomnteftyonjyiojxnznfeubjctijmnyaanwgsphieqhpgsoutbbxycjaxrklekogakpsbwdimkxvelpyosvmxgnuxzgejwmjgbehxhpmtohzbyxqsvepbrmzsufcqrnwttfscxgxlpxnpufirjxtdjuvfzzvqprlizelwmkjchwtcdbvpbosminsjpncehnmgtzegknkrmdvrhrgihywsoobdedhltvtmxzuhmeaakysrpybyzxqnouqszzfswahtzbanidoubilsgoqfnjubdmvclaxkaedbfeppj", "fjknwevk"))
+	t.Log(minWindowV2("cabwefgewcwaefgcf", "cae"))
 }
